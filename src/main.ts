7ad95c2c2eca2ca5e3213d59113c4807abdad8dd
井原 樹/Miki Ihara <kimity0824@icloud.com>
2023-05-05 03:39:03 +0900

    cloud runデプロイ準備

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { MyLogger } from './logger/logger.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule, {
+    logger: new MyLogger(),
+  });
+  const port = Number(process.env.PORT) || 4000; // Cloud Run の要件。環境変数PORTで起動するようにする。
+  await app.listen(port, '0.0.0.0'); // '0.0.0.0' を追加して外部からのアクセスを受け入れる。
 }
 bootstrap();
